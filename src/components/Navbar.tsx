a6769e62f47a623f44d9129afc7fc6b446921ac7
Erik Peteri <peteri.erik@gmail.com>
2024-01-08 20:52:24 +0200

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,63 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="text-white relative">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+    <nav className="relative">
+      <div className="max-w-screen-xl flex items-center justify-end mx-auto p-4">
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+
+        <div className="hidden w-full md:flex md:w-auto" id="navbar-default">
+          <NavLink
+            to="/"
+            className="py-4 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-4 dark:text-white md:dark:text-blue-500"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className="py-4 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to="/projects"
+            className="py-4 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          >
+            Projects
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            className="py-4 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          >
+            Contact
+          </NavLink>
+        </div>
+      </div>
     </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
       },
+      colors: {
+        "secondary-color": "var(--secondary-color)"
+      },
     },
   },
   plugins: [],
