55cfd9e1d679fdde63be584c93f83ea7de03445e
Erik Peteri <peteri.erik@gmail.com>
2024-01-11 21:27:47 +0200

    change page layout to use flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import Hero from "./components/Hero";
-import NavBar from "./components/Navbar";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
+import Content from "./components/Content";
+
 
 const App = () => {
   return (
     <div>
-      <Hero />
-      <NavBar />
-      <Footer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="flex flex-col h-screen">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
     </div>
   );
 };
