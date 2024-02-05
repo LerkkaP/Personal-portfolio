df54bae01f354bbedae999380cfec9da1779db7d
Erik Peteri <peteri.erik@gmail.com>
2024-02-05 19:39:08 +0200

    add alternating shadow effect on card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,11 +9,11 @@ const Card = () => {
       setFlip(!flip)
     }
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-md shadow-blue-400 mx-5">
+        <div className={`max-w-sm rounded overflow-hidden mx-5 ${flip ? 'shadow-white':'shadow-blue'}`}>
             <div className="group h-96 w-80">
                 <div className={`relative h-full w-full rounded-xl card ${flip? 'cardFlip':''}`}>
                 <div className="absolute inset-0 text-black front">
-                <div className="flex min-h-full flex-col items-center justify-center">
+                <div className="flex min-h-full flex-col items-center justify-center ">
                     <h1 className="text-3xl font-bold text-white">Flappy Bird</h1>
                     <button className="bg-white text-black mt-20 rounded-sm py-1 px-4">
                                 <a href="https://github.com/LerkkaP/flappy-bird">
@@ -30,7 +30,7 @@ const Card = () => {
                             <span className="inline-block bg-gray-200 rounded-sm px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">MatplotLib</span>
                             <span className="inline-block bg-gray-200 rounded-sm px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">TinyDB</span>
                         </div>
-                        <button className='absolute bottom-2 right-2' onClick={handleFlip}><FlipToFrontIcon color='primary'/></button>                
+                        <button className='absolute bottom-2 right-2' onClick={handleFlip}><FlipToFrontIcon /></button>                
 
                         <div className="mb-4">
                         </div>
